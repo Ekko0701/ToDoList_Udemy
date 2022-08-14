669d06d29b0e179bd3e668ec398d0a88507f5ef0
Ekko0701 <djekko0701@gmail.com>
2022-08-14 21:53:34 +0900

    Encoding & Decoding Data with NSCoder

---
+++ ToDoList_Udemy/Data Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  ToDoList_Udemy
+//
+//  Created by Ekko on 2022/08/14.
+//
+
+import Foundation
+
+class Item: Codable {
+    var title: String = ""
+    var done: Bool = false
+}
